cfaa5eb38beec3cac892bcef1bcca533b48cb6f1
YannTangKow <93475812+YannTangKow@users.noreply.github.com>
2024-01-26 16:48:53 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
       },
     ]
   },
