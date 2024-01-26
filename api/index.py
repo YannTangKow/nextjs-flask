d2795bbc1157eaa7ff3ca43e69acc98f7e5d7eca
YannTangKow <93475812+YannTangKow@users.noreply.github.com>
2024-01-26 16:42:51 +0800

    change to python

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
+@app.route('/api/python', methods=['GET'])
 def hello_world():
     return "Hello, World!"
 
